ef0592bd735a5e8b581b172edb3523ec79c032d5
Chinmay Sai Vemuri <55590938+rashtrakoff@users.noreply.github.com>
2023-06-08 16:01:58 +0530

    Pinning old deployments

--- deployment-scripts/configs/config.optimism.ts
+++ deployment-scripts/configs/config.optimism.ts
@@ -7,6 +7,6 @@ export const config = {
     "MTA": "0x929B939f8524c3Be977af57A4A0aD3fb1E374b50",
     "MTy": "0x0f6eae52ae1f94bc759ed72b201a2fdb14891485", // mStable Treasury Yield (https://app.dhedge.org/vault/0x0f6eae52ae1f94bc759ed72b201a2fdb14891485)
     "OptimismMultisig": "0x352Fb838A3ae9b0ef2f0EBF24191AcAf4aB9EcEc", // Used for burning MTA on Optimism.
-    "ExchangePrice": ethers.utils.parseUnits("318", 14), // $0.03
+    "ExchangePrice": ethers.utils.parseUnits("318", 14), // $0.0318
     "MaxTokenPriceDrop": 10, // 10:10000 => 0.1% max price drop acceptable
 }
