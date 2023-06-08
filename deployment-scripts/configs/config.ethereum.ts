867ebf6779338cdb740e54efda4510f5af53dd7c
Chinmay Sai Vemuri <55590938+rashtrakoff@users.noreply.github.com>
2023-06-08 11:13:52 +0530

    Updated config values

--- deployment-scripts/configs/config.ethereum.ts
+++ deployment-scripts/configs/config.ethereum.ts
@@ -3,5 +3,5 @@
 export const config = {
     "L1CrossDomainMessenger": "0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1",
     "MTA": "0xa3BeD4E1c75D00fa6f4E5E6922DB7261B5E9AcD2",
-    "CrossChainGasLimit": 1_920_000
+    "CrossChainGasLimit": 2_500_000
 }
--- deployment-scripts/configs/config.optimism.ts
+++ deployment-scripts/configs/config.optimism.ts
@@ -1,11 +1,12 @@
 import { ethers } from "ethers";
 
-// Deployment arguments for L1Comptroller contract.
+// Deployment arguments for L2Comptroller contract.
+
 export const config = {
     "L2CrossDomainMessenger": "0x4200000000000000000000000000000000000007",
     "MTA": "0x929B939f8524c3Be977af57A4A0aD3fb1E374b50",
     "MTy": "0x0f6eae52ae1f94bc759ed72b201a2fdb14891485", // mStable Treasury Yield (https://app.dhedge.org/vault/0x0f6eae52ae1f94bc759ed72b201a2fdb14891485)
     "OptimismMultisig": "0x352Fb838A3ae9b0ef2f0EBF24191AcAf4aB9EcEc", // Used for burning MTA on Optimism.
-    "ExchangePrice":ethers.utils.parseUnits("3", 16), // $0.03
+    "ExchangePrice": ethers.utils.parseUnits("318", 14), // $0.03
     "MaxTokenPriceDrop": 10, // 10:10000 => 0.1% max price drop acceptable
 }
